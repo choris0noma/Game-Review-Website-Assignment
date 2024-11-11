d183165188b306b36c95de3c58711c0cbe33dc5f
Alish006 <153196767+Alish006@users.noreply.github.com>
2024-11-11 23:11:36 +0500

    news
    .

--- js/news.js
+++ js/news.js
@@ -212,7 +212,90 @@ var usernameSaved = localStorage.getItem('username');
     }
 }
 
+// Sign up
+
+let form = document.getElementById("signUpForm");
+
+function openForm() 
+{
+    form.style.display = "block";
+}
+  
+function closeForm() 
+{
+    form.style.display = "none";
+}
+
+function validateEmail() 
+{
+    var errorMessage = document.getElementById("error-message");
+
+    var emailValue = document.getElementById("email").value;
+    var passwordValue = document.getElementById("psw").value;
+
+    var isValid = true;
+    
+    if (emailValue.includes('@')) 
+    {
+        errorMessage.style.display = 'none';
+        errorMessage.textContent += ''; 
+    } 
+    else 
+    {
+        errorMessage.style.display = 'block';
+        errorMessage.textContent += 'Email must contain an "@" symbol';
+        isValid = false;
+    }
+    if (passwordValue.length >= 8) 
+    {
+        errorMessage.style.display = 'none';
+        errorMessage.textContent += ''; 
+    }
+    else
+    {
+        errorMessage.style.display = 'block';
+        errorMessage.textContent += '\nPassword must be at least 8 characters long';
+        isValid = false;
+    }
+    if(isValid)
+    {
+        localStorage.setItem('username', emailValue);
+        displayShowAccount(username);
+    }
+    return isValid; 
+}
+
+function checkLogin() {
+    const username = localStorage.getItem('username')
+    if (username) {
+        displayShowAccount(username);
+        
+    } else {
+        displayLogin();
+   
+    }
+}
+
+function displayLogin() {
+    document.querySelector(".showAccountBtn").style.display = 'none';
+    document.querySelector(".loginBtn").style.display = 'block';
+
+    document.getElementById('signUpForm').style.display = 'none'
+}
+
+function displayShowAccount(username) {
+    document.querySelector(".showAccountBtn").style.display = 'block';
+    document.querySelector(".loginBtn").style.display = 'none';
+
+    document.getElementById('signUpForm').style.display = 'none';
+}
+
+
+document.getElementById('show-account-button').addEventListener('click', () => {
+    document.getElementById('account-popup').style.display = 'block'
+})
 
 
 
+checkLogin()
 
